e8d3f9b47985be11db2134ba21ee389e73030653
jibranthecoder <blackzero73@gmail.com>
2020-11-06 15:18:31 +0500

    deploying

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <>
+        <h2>
+            Expense Tracker 125
+        </h2>
+            
+        </>
+    )
+}
